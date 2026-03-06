18ee4e055ccae5692e78371f7f3ac0ee95a83013
Xnodes Development <xnodesdev@gmail.com>
2026-03-06 15:16:26 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,3 +1,10 @@
+/**
+ * ◈ GARFIELD-V11-CORE ◈
+ * * This architectural core is engineered for performance.
+ * Designed & Developed by Tharindu Liyanage
+ * * © 2026 Xnodes Laboratory. All rights reserved.
+ * ---------------------------------------------------------
+ */
 const { commands } = require('../command')
 const { facebook } = require('another-fb-video-downloader')
 const fs    = require('fs')
