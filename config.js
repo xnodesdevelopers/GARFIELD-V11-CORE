e2cbce087cfd506b60969efd82e7149bea777729
Xnodes Development <xnodesdev@gmail.com>
2026-03-06 14:38:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   MODE: 'public',
 
   READ_MESSAGE:   'false',
-  GEMINI_API_KEY: process.env.GEMINI_API_KEY || 'AIzaSyAl90hhWpJvPSH1Oo4biJyi2pHOdcfEtz0',
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || ' ',
 
   BLOCK_COUNTRIES: [
     { code: '212', name: 'Morocco', minLen: 12 },
